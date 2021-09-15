1283a7b85108ba82bbdd40363c1d5a3f5dd829bf
TrungDucMai <trungmaiduc196@gmail.com>
2021-09-15 22:43:41 +0700

    taomoi

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+    @RequestMapping("/employee")
+    public class EmployeeController {
+
+        @GetMapping( "/showForm")
+        public String showForm(ModelMap model) {
+            model.addAttribute("employee", new Employee());
+            return "create";
+        }
+
+        @PostMapping("/addEmployee")
+        public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+            model.addAttribute("name", employee.getName());
+            model.addAttribute("contactNumber", employee.getContactNumber());
+            model.addAttribute("id", employee.getId());
+            return "info";
+        }
+    }
